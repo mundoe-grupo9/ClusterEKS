c51971a7003376f0609cbe6fd09cc01ab299042b
Jonathan <jonathan.medula@gmail.com>
2024-09-24 21:01:13 -0300

    primerDeploy03

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,121 +1,20 @@
-module "eks" {
-  source = "terraform-aws-modules/eks/aws"
+resource "aws_eks_cluster" "example" {
+  name     = "my-cluster"
+  role_arn = "arn:aws:iam::992382474736:role/my-cluster-cluster-20240924235255190800000002"
 
-  cluster_name                    = "my-cluster"
-  cluster_version                 = "1.30"
-  cluster_endpoint_private_access = true
-  cluster_endpoint_public_access  = true
-
-  cluster_addons = {
-    coredns = {
-      resolve_conflicts = "OVERWRITE"
-    }
-    kube-proxy = {}
-    vpc-cni = {
-      resolve_conflicts = "OVERWRITE"
-    }
+  vpc_config {
+    subnet_ids = [aws_subnet.private_subnet.id, aws_subnet.public_subnet.id]
   }
 
-  vpc_id     = aws_vpc.my_vpc.id
-  subnet_ids = [aws_subnet.private_subnet.id, aws_subnet.public_subnet.id]
-
-  # Self Managed Node Group(s)
-  self_managed_node_group_defaults = {
-    instance_type                          = "t2.medium"
-    update_launch_template_default_version = true
-  }
-
-  self_managed_node_groups = {
-    one = {
-      name = "spot-1"
-
-      public_ip    = true
-      max_size     = 2
-      desired_size = 2
-
-      use_mixed_instances_policy = true
-      mixed_instances_policy = {
-        instances_distribution = {
-          on_demand_base_capacity                  = 0
-          on_demand_percentage_above_base_capacity = 10
-          spot_allocation_strategy                 = "capacity-optimized"
-        }
-
-        override = [
-          {
-            instance_type     = "t2.medium"
-            weighted_capacity = "1"
-          },
-          {
-            instance_type     = "t2.medium"
-            weighted_capacity = "1"
-          },
-        ]
-      }
-
-      
-    }
-  }
-
-  # EKS Managed Node Group(s)
-  eks_managed_node_group_defaults = {
-    ami_type               = "AL2_x86_64"
-    disk_size              = 50
-    instance_types         = ["t2.medium"]
-    vpc_security_group_ids = [aws_security_group.my_sg.id]
-  }
-
-  eks_managed_node_groups = {
-    blue = {}
-    green = {
-      min_size     = 1
-      max_size     = 10
-      desired_size = 1
-
-      instance_types = ["t2.medium"]
-      capacity_type  = "SPOT"
-      labels = {
-        Environment = "test"
-        GithubRepo  = "terraform-aws-eks"
-        GithubOrg   = "terraform-aws-modules"
-      }
-      
-      tags = {
-        ExtraTag = "example"
-      }
-    }
-  }
-
-  # Fargate Profile(s)
-  fargate_profiles = {
-    default = {
-      name = "default"
-      selectors = [
-        {
-          namespace = "kube-system"
-          labels = {
-            k8s-app = "kube-dns"
-          }
-        },
-        {
-          namespace = "default"
-        }
-      ]
-
-      tags = {
-        Owner = "test"
-      }
-
-      timeouts = {
-        create = "20m"
-        delete = "20m"
-      }
-    }
-  }
-
-  tags = {
-    Environment = "dev"
-    Terraform   = "true"
-  }
+  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
+  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
+  
 }
 
+output "endpoint" {
+  value = aws_eks_cluster.example.endpoint
+}
+
+#output "kubeconfig-certificate-authority-data" {
+ # value = aws_eks_cluster.example.certificate_authority[0].data
+ #}
